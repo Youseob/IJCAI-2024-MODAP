1abb32d65352007d2a3f289b32d640269710f970
Youseob <cys9506@kaist.ac.kr>
2023-01-19 04:45:20 +0000

    bash file

--- OfflineRL/lauch_test.sh
+++ OfflineRL/lauch_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for dataset in $seq walker2d-random; do
-    for seed in $seed 0; do
+    for seed in $seq 0; do
         python examples/train_d4rl.py --algo_name=bayrl_cvar --exp_name=train --train_dynamics=True --task=d4rl-$dataset-v2 --dynamics_save_path=$dataset-20-seed-$seed.th --seed=$seed
     for epsilon in $seq 0.9 0.7 0.5; do
             for beta in $seq 100 1 10; do

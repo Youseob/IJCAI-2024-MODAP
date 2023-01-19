06435f2116a76362c8f37a2bf6ac83651a82e01e
Youseob <cys9506@kaist.ac.kr>
2023-01-19 04:30:09 +0000

    bash file

--- OfflineRL/lauch_test_gpu1.sh
+++ OfflineRL/lauch_test_gpu1.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 for dataset in $seq walker2d-random; do
     for seed in $seed 0; do
-    for epsilon in $seq 0.9 0.7 0.5 0.3; do
+    CUDA_VISIBLE_DEVICES=1 python examples/train_d4rl.py --algo_name=bayrl_cvar --exp_name=train --train_dynamics=True --task=d4rl-$dataset-v2 --dynamics_save_path=$dataset-20-seed-$seed.th --seed=$seed
+    for epsilon in $seq 0.9 0.7 0.5; do
             for beta in $seq 100 1 10; do
             CUDA_VISIBLE_DEVICES=1 python examples/train_d4rl.py --algo_name=bayrl_cvar --exp_name=bayrl_cvar_10-$epsilon-beta-$beta --task=d4rl-$dataset-v2 --dynamics_path=$dataset-20-seed-$seed.th --out_train_epoch=300 --worst_percentil=$epsilon --soft_belief_temp=$beta --seed=$seed
             done

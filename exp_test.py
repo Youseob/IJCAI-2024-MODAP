821957f6ee9ab8228b292c421d8e25eef7de4173
Youseob <cys9506@kaist.ac.kr>
2023-01-31 08:56:25 +0000

    modify

--- exp_test.py
+++ exp_test.py
@@ -1,10 +1,10 @@
 import wandb
-import gym
-import d4rl
+# import gym
+# import d4rl
 
 for env in ['halfcheetah']:
     for dset in ['medium', 'medium-expert']:
         dset_name = env+'_'+dset.replace('-', '_')+'-v2'
         env_name = dset_name.replace('_', '-')
-        env = gym.make(env_name)
-        dataset = env.get_dataset()
+        gym_env = gym.make(env_name)
+        dataset = gym_env.get_dataset()

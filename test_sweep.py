a5febd8d3312d065853cb504baa2c691b14f022f
Youseob <cys9506@kaist.ac.kr>
2023-02-20 16:36:17 +0000

    modify test_sweeep.py

--- test_sweep.py
+++ test_sweep.py
@@ -2,6 +2,5 @@ import os
 
 
 dataset = str(os.environ.get('dataset', "None"))
-learning_rate = float(os.environ.get('learning_rate', 0.01)
-# 입력받은 인자값 출력
+learning_rate = float(os.environ.get('learning_rate', 0.01))
 print(learning_rate)

c14f1e9e8871636e257efef60562d2c429266702
Youseob <cys9506@kaist.ac.kr>
2023-02-20 16:08:46 +0000

    modify test_sweeep.py

--- test_sweep.py
+++ test_sweep.py
@@ -1,12 +1,6 @@
-import argparse
+import os
 
-parser = argparse.ArgumentParser(description='Argparse Tutorial')
-parser.add_argument('--epoch', type=int, default=150)
-parser.add_argument('--batch_size', type=int, default=128)
-parser.add_argument('--lr_initial', type=float, default=0.1)
-parser.add_argument('--dataset', type=str, default="walker2d-random")
 
-args    = parser.parse_args()
-
+dataset = float(os.environ.get('dataset', "None"))
 # 입력받은 인자값 출력
-print(args.dataset)
+print(dataset)

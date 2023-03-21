ec8b4a44427cc2388a43f0cd3a5b6d1a8d922ecf
Youseob <cys9506@kaist.ac.kr>
2023-03-21 11:47:17 +0000

    fix bug bash file

--- OfflineRL/launch_adv.sh
+++ OfflineRL/launch_adv.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
-python examples/train_d4rl.py --algo_name=adv_bayrl_v2 \
-                              --exp_name=20220307-adv-kl-$temp-r_scale-$r_scale \
+# dataset=walker2d-random
+num_model=100
+# num_traj_infer=100
+# add_value=True
+cal=True
+r_scale=10
+belief_mode=bay
+dataset=walker2d-random
+seed=42
+horizon_step=5
+
+WANDB_MODE=disabled python examples/train_d4rl.py --algo_name=adv_bayrl_v3 \
+                              --exp_name=adv-bay-r_scale-$r_scale \
                               --task=d4rl-$dataset-v2 \
                               --transition_init_num=110 \
                               --transition_select_num=$num_model \
-                              --dynamics_path=/model/$dataset-$num_model-seed-$seed.th \
+                              --dynamics_path=/root/$dataset-$num_model-seed-$seed.th \
                               --traj_num_to_infer=$num_traj_infer \
                               --belief_update_mode=$belief_mode \
-                              --temp=$temp \
                               --add_value_to_rt=$add_value \
                               --b_lr=1e-4 \
+                              --horizon=$horizon_step
                               --reward_scale=$r_scale \
                               --calibration=$cal \
                               --seed=$seed

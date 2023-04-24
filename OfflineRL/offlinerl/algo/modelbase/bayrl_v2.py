9efd6abd3cf96966aaec5a7a8c47b855441145e9
Youseob <cys9506@kaist.ac.kr>
2023-04-24 11:48:16 +0000

    add save path

--- OfflineRL/offlinerl/algo/modelbase/bayrl_v2.py
+++ OfflineRL/offlinerl/algo/modelbase/bayrl_v2.py
@@ -72,7 +72,7 @@ class AlgoTrainer(BaseAlgo):
         self.args = args        
         wandb.init(
             config=self.args,
-            project='420' + self.args["task"], # "d4rl-halfcheetah-medium-v2"
+            project='bayrl' + self.args["task"], # "d4rl-halfcheetah-medium-v2"
             group=self.args["algo_name"], # "maple"
             name=self.args["exp_name"], 
             id=str(uuid.uuid4())

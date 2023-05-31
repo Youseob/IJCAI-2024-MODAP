46864442c1b920a95c3f9a0335926ce6b056ffe0
Youseob <cys9506@kaist.ac.kr>
2023-05-31 11:10:46 +0000

    fix bug for vessl

--- OfflineRL/launch_maple.sh
+++ OfflineRL/launch_maple.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-dataset=halfcheetah-medium
-num_model=5
-seed=42
-H=5
+# dataset=halfcheetah-medium
+# num_model=5
+# seed=42
+# H=5
 python examples/train_d4rl.py --algo_name=maple \
                               --exp_name=maple-$num_model-mode-$seed \
                               --task=d4rl-$dataset-v2 \

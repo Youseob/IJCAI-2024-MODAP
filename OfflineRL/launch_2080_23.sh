13a8a7a41a319e2e558abeff1e26d14f57d0f7bc
Youseob <cys9506@kaist.ac.kr>
2023-07-25 06:38:15 +0000

    vessl exl for walker2d-medium

---
+++ OfflineRL/launch_2080_23.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# dataset=hopper-medium
+# init_num_model=7
+# num_model=5
+# seed=42
+# H=10
+# epoch_per_div_update=5
+# out_epochs=400
+# real_data_ratio=0.2
+# weight=100
+lam=0
+reward_type=mean_reward
+python required.py
+
+python examples/train_d4rl.py --algo_name=maple_div_v1 \
+                              --exp_name=div-$num_model-model-$H-H-$weight-dw-$real_data_ratio-ratio-$epoch_per_div_update-update-2 \
+                              --task=d4rl-$dataset-v2 \
+                              --transition_init_num=$init_num_model \
+                              --transition_select_num=$num_model \
+                              --dynamics_save_path=/output/$dataset-$num_model-2-ckpt.th \
+                              --horizon=$H \
+                              --epoch_per_div_update=$epoch_per_div_update \
+                              --out_epochs=$out_epochs \
+                              --real_data_ratio=$real_data_ratio \
+                              --reward_type=$reward_type \
+                              --lam=$lam \
+                              --diversity_weight=$weight \
+                              --save_path=/output/div-$num_model-model-$H-H-$weight-dw-2-ckpt.th \
+                              --seed=2 & \
+python examples/train_d4rl.py --algo_name=maple_div_v1 \
+                              --exp_name=div-$num_model-model-$H-H-$weight-dw-$real_data_ratio-ratio-$epoch_per_div_update-update-3 \
+                              --task=d4rl-$dataset-v2 \
+                              --transition_init_num=$init_num_model \
+                              --transition_select_num=$num_model \
+                              --dynamics_save_path=/output/$dataset-$num_model-3-ckpt.th \
+                              --horizon=$H \
+                              --epoch_per_div_update=$epoch_per_div_update \
+                              --out_epochs=$out_epochs \
+                              --real_data_ratio=$real_data_ratio \
+                              --reward_type=$reward_type \
+                              --lam=$lam \
+                              --diversity_weight=$weight \
+                              --save_path=/output/div-$num_model-model-$H-H-$weight-dw-3-ckpt.th \
+                              --seed=3
